f5bbf939addcd6ff7a02a1136951af9ff3f0b3c9
Luke Dreyer <lukepdreyer@gmail.com>
2018-11-19 23:05:56 -0800

    add in basic logic for user choosing interactive dialogue options based on bot response to user input

--- public/io_manager.js
+++ public/io_manager.js
@@ -16,8 +16,6 @@ function getFoodData(filter) {
       .value.replace(/\n/gi, " ")
       .replace(/  /gi, " ");
     filtered = filter(food_text);
-    
-
 
     document.getElementById("text-display").innerHTML += `<div class="user-input">${food_text}</div>`;
     if (filtered.type === 0) xhrFoodReq(filtered.text);
@@ -34,15 +32,38 @@ function xhrFoodReq(food){
     xhttp.onreadystatechange = function (e, res) {
         // if (this.readyState == 4 && this.status == 200) {
         // console.log(res);
+        
         if (this.response.length > 0 && !alreadyPosted) {
+            
             alreadyPosted = true;
-            document.getElementById("text-display").innerHTML += `<div class="bot-input">${this.response}</div>`;
+
+            let items = JSON.parse(this.response);
+            let opts = "";
+            for(let i = 0; i < items.length; ++i){
+                
+                window.localStorage.setItem(i, JSON.stringify(items[i]));
+                opts += `<option value="${i}">${items[i].Shrt_Descrpt}</option>`;
+
+            }
+            document.getElementById("text-display").innerHTML += `<div class="bot-input">
+                Which one was it?
+                <select id="chooseCurFood">
+                ${opts}
+                </select>
+                <button onClick="assignFood()">Choose</button>
+            </div>`;
         }
     }
     xhttp.open("POST", "foodtext", true);
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     let params = { food: food };
     xhttp.send(JSON.stringify(params));
+}
+
+function assignFood(){
+    let choice = document.getElementById("chooseCurFood");
+    choice.replaceWith(`<span>${window.localStorage.getItem(choice.selectedIndex)}</span>`);
+    window.localStorage.clear();
 }
 
 // function updateAll(data){
